d5af914932a05a0e6c2b1fd49d05f15ea4554a10
tsents <tsents.sagie@gmail.com>
2025-06-24 22:58:31 +0300

    Improve doc, before doing big changes.

--- go_parse/myjson/parallel_parse.go
+++ go_parse/myjson/parallel_parse.go
@@ -16,26 +16,37 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
+//Define the json engine for this package, and all subpackages that will inherit it.
 var Myjson = jsoniter.ConfigCompatibleWithStandardLibrary
 
 
 type AnyJSON map[string]any;
 type ActionFunc[T any] func([]byte) (T, error);
-type boundedActionFunc func([]byte);
+type bindedActionFunc func([]byte);
 type ManagerFunc[T any] func(<-chan T);
 
 const (
 	workerCount   = 8  // adjust based on CPU cores
 	channelBuffer = 16 // buffer size for work queue
 	readWorkersCount = 2 // adjust based on usecase? there are upsides/downsides for higher/lower.
+	httpTimeout = 180 // seconds
 )
 
 /*
  Parses a list of files in paralle. The action in parsing
  is the action "action", and additionaly a manager goroutine
- is create (once!) with a in channel from the action goroutines.
+ is create (once!) with a in channel from the action
+ goroutines to allow communication (of type T).
+ The file format is expected to be NDJSON (new line delimited json),
+ that is compressed using gz. (thus filename.json.jz).
+
+Paramaters:
+	- files[]		An array of files, serving as the source. can be real/url.
+	- action		The action to preform on each json entry in the file.
+	- manager 		The function that collects all the output from the functions, and used to give the final result.
+	- sourceType	The type of the file source. either a route to a real file. or http. (takes "file"/"http") 
 */
-func ParseInParallel[T any](files []string, action ActionFunc[T], manager ManagerFunc[T]) {
+func ParseInParallel[T any](files []string, action ActionFunc[T], manager ManagerFunc[T], sourceType string) {
 	var wg sync.WaitGroup
 	workChan := make(chan T)
 
@@ -57,7 +68,7 @@ func ParseInParallel[T any](files []string, action ActionFunc[T], manager Manage
 	fileChan := make(chan string)
 
 	// Bound action to the work channel
-	var boundedAction boundedActionFunc = func(line []byte) {
+	var boundedAction bindedActionFunc = func(line []byte) {
 		retValue, err := action(line)
 		if (err != nil) {
 			return // Can add stuff later for error printing.
@@ -72,14 +83,14 @@ func ParseInParallel[T any](files []string, action ActionFunc[T], manager Manage
 			for filename := range fileChan {
 				file, err := os.Open(filename)
 				if err != nil {
-					fmt.Printf("Failed to open file: %v", err)
+					fmt.Fprintf(os.Stderr, "Failed to open source: %v\n", err);
 					continue
 				}
 
 				err = ProcessNDJSONInParallel(file, boundedAction)
 				file.Close() // close explicitly
 				if err != nil {
-					fmt.Printf("Error processing NDJSON: %v", err)
+					fmt.Fprintf(os.Stderr, "Error processing NDJSON: %v\n", err)
 					continue
 				}
 
@@ -116,7 +127,7 @@ func ParseInParallel[T any](files []string, action ActionFunc[T], manager Manage
   The user should make sure that the action function has no race conditions,
   and to capture its output, it should output into a channel within itself.
  */
-func ProcessNDJSONInParallel(originalReader io.Reader, action boundedActionFunc) error {
+func ProcessNDJSONInParallel(originalReader io.Reader, action bindedActionFunc) error {
 	gz, err := gzip.NewReader(originalReader)
 	if err != nil {
 		return fmt.Errorf("gzip reader error: %v", err)
@@ -178,3 +189,4 @@ func fetchWithTimeout(url string, timeoutSeconds int) (io.Reader, error) {
 	// The caller is responsible for closing resp.Body
 	return resp.Body, nil
 }
+
